002b9b1e3202cf967e736e3b389f0189961af19f
Glitch (vaulted-clammy-effect) <none>
2022-02-13 10:25:08 +0000

    💏🗽 Checkpoint ./commands/ekonomi/slots.js:25470421/2504 ./commands/extras/hesapla.js:25470421/11274

--- commands/ekonomi/slots.js
+++ commands/ekonomi/slots.js
@@ -4,27 +4,36 @@ const { Command } = require('discord.js-commando');
 const { stripIndents } = require('common-tags');
 const slots = ['🥒', '🍎', '🍅', '🍋', '🐻'];
 
-exports.run = function(client, message) {
+exports.run = function(client, message, args) {
 
+  var oynamak = args[0]
+ 
     var slot1 = slots[Math.floor(Math.random() * slots.length)];
     var slot2 = slots[Math.floor(Math.random() * slots.length)];
     var slot3 = slots[Math.floor(Math.random() * slots.length)];
 
-     var paras = ["320000"];
-      var parasik = paras[Math.floor(Math.random() * paras.length)];
+
+if(!oynamak) return message.channel.send('Oynayacağınız Para Miktarını Giriniz')
+
+let parapara =  db.fetch(`para_${message.author.id}`) || 0  
+
 
       
     if (slot1 === slot2 && slot1 === slot3) {
-       db.add(`para_${message.author.id}`, parasik)
+       db.add(`para_${message.author.id}`, oynamak + oynamak)
         message.channel.send(stripIndents`
         ${slot1} : ${slot2} : ${slot3}
         Tebrikler, kazandınız!
+
+        Paran = ${parapara}
         `);
     } else {
-      db.subtract(`para_${message.author.id}`,640000)
+      db.subtract(`para_${message.author.id}`,oynamak)
         message.channel.send(stripIndents`
         ${slot1} : ${slot2} : ${slot3}
         Eyvah, kaybettin!
+
+        Paran = ${parapara}
         `);   
     }
 
