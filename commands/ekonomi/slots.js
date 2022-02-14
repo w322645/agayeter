ad668cdee56af0798e33ff3a7f041416f192b988
Glitch (vaulted-clammy-effect) <none>
2022-02-14 14:53:42 +0000

    🎿🌇 Checkpoint ./commands/ekonomi/slots.js:25470421/1435

---
+++ commands/ekonomi/slots.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const db = require('quick.db')
+const math = require("math-expression-evaluator");
+const { Command } = require('discord.js-commando');
+const { stripIndents } = require('common-tags');
+const slots = ['🥒', '🍎', '🍅', '🍋', '🐻'];
+
+exports.run = function(client, message, args) {
+
+  var oynamak = args[0]
+ 
+    var slot1 = slots[Math.floor(Math.random() * slots.length)];
+    var slot2 = slots[Math.floor(Math.random() * slots.length)];
+    var slot3 = slots[Math.floor(Math.random() * slots.length)];
+
+
+    if (isNaN(oynamak)) return message.reply('Para Miktarını Nası Sayısız Yazıcaksın?')
+
+let parapara = db.fetch(`para_${message.author.id}`) || 0  
+
+if (parapara < oynamak) return message.reply('Para Miktarından Büyük bir Para Miktarıyla Slot Oyununu Oynayamassın :(')
+      
+    if (slot1 === slot2 && slot1 === slot3) {
+       db.add(`para_${message.author.id}`, +oynamak)
+        message.channel.send(stripIndents`
+        ${slot1} : ${slot2} : ${slot3}
+        Tebrikler, kazandınız!
+
+   
+        `);
+    } else {
+      db.add(`para_${message.author.id}`,-oynamak)
+        message.channel.send(stripIndents`
+        ${slot1} : ${slot2} : ${slot3}
+        Eyvah, kaybettin!
+
+        
+        `);   
+    }
+  
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'slot',
+  description: 'Slots oyunu oynatır',
+  usage: 'slot'
+};
